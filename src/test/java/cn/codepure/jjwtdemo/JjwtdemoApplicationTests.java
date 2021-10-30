62eb23e0334e76110b5d7053aef55763d6470c32
wys010812 <209306976@qq.com>
2021-10-30 23:07:33 +0800

    Token生成测试

--- src/test/java/cn/codepure/jjwtdemo/JjwtdemoApplicationTests.java
+++ src/test/java/cn/codepure/jjwtdemo/JjwtdemoApplicationTests.java
@@ -1,13 +1,39 @@
 package cn.codepure.jjwtdemo;
 
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.Base64Codec;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Base64;
+import java.util.Date;
 
 @SpringBootTest
 class JjwtdemoApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void testCreateToken() {
+        // 1. 创建JwtBuilder
+        JwtBuilder jwtBuilder = Jwts.builder()
+                // 声明的标识 {"jti":"8888"}
+                .setId("8888")
+                // 主体用户 {"sub":"Rose"}
+                .setSubject("Rose")
+                // 创建日期 {"ita":"xxxxx"}
+                .setIssuedAt(new Date())
+                .signWith(SignatureAlgorithm.HS256, "xxxxxxx");
+        // 2. 获取jwt的token
+        String token = jwtBuilder.compact();
+        System.out.println(token);
+
+        System.out.println("================");
+        String[] split = token.split("\\.");
+        System.out.println(Base64Codec.BASE64.decodeToString(split[0]));
+        System.out.println(Base64Codec.BASE64.decodeToString(split[1]));
+        // 签名无法解密
+        System.out.println(Base64Codec.BASE64.decodeToString(split[2]));
     }
 
 }
