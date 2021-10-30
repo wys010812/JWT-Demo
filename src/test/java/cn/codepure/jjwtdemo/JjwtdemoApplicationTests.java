ca1c76b04922838cce9a576e0d0010b381aa309c
wys010812 <209306976@qq.com>
2021-10-30 23:35:18 +0800

    自定义声明

--- src/test/java/cn/codepure/jjwtdemo/JjwtdemoApplicationTests.java
+++ src/test/java/cn/codepure/jjwtdemo/JjwtdemoApplicationTests.java
@@ -110,4 +110,58 @@ class JjwtdemoApplicationTests {
         System.out.println("过期时间："+simpleDateFormat.format(claims.getExpiration()));
         System.out.println("现在时间："+simpleDateFormat.format(new Date()));
     }
+
+    /**
+     * 创建token （自定义声明）
+     */
+    @Test
+    public void testCreateTokenByClaims() {
+        // 当前系统时间
+        long now = System.currentTimeMillis();
+        // 过期时间 获取时间为毫秒
+        long exp = now + 60 * 1000;
+        // 1. 创建JwtBuilder
+        JwtBuilder jwtBuilder = Jwts.builder()
+                // 声明的标识 {"jti":"8888"}
+                .setId("8888")
+                // 主体用户 {"sub":"Rose"}
+                .setSubject("Rose")
+                // 创建日期 {"ita":"xxxxx"}
+                .setIssuedAt(new Date())
+                // 设置加密算法和盐
+                .signWith(SignatureAlgorithm.HS256, "xxxxxxx")
+                // 自定义声明
+                .claim("roles", "admin")
+                .claim("logo", "xxx.jpg");
+                // 还可以使用Map声明
+                // .addClaims()
+        // 2. 获取jwt的token
+        String token = jwtBuilder.compact();
+        System.out.println(token);
+
+        System.out.println("================");
+        String[] split = token.split("\\.");
+        System.out.println(Base64Codec.BASE64.decodeToString(split[0]));
+        System.out.println(Base64Codec.BASE64.decodeToString(split[1]));
+        // 签名无法解密
+        System.out.println(Base64Codec.BASE64.decodeToString(split[2]));
+    }
+
+    /**
+     * 解析token （自定义声明）
+     */
+    @Test
+    public void testParseTokenByClaims() {
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI4ODg4Iiwic3ViIjoiUm9zZSIsImlhdCI6MTYzNTYwNzk5NCwicm9sZXMiOiJhZG1pbiIsImxvZ28iOiJ4eHguanBnIn0.LMl3plwxlZMwuj6NITxNGocVQp_7vtrxxXGyKLGxbvE";
+        // 解析Token 获取负载中声明的对象
+        Claims claims = Jwts.parser()
+                .setSigningKey("xxxxxxx")
+                .parseClaimsJws(token)
+                .getBody();
+        System.out.println("id:" + claims.getId());
+        System.out.println("subject:" + claims.getSubject());
+        System.out.println("issuedAt:" + claims.getIssuedAt());
+        System.out.println("Roles:" + claims.get("roles"));
+        System.out.println("LOGO:" + claims.get("logo"));
+    }
 }
