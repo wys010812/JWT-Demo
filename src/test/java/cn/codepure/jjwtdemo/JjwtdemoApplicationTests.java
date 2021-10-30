3cb27591fe7945092a027dc9dc10e74ea2fe7071
wys010812 <209306976@qq.com>
2021-10-30 22:39:42 +0800

    创建项目

---
+++ src/test/java/cn/codepure/jjwtdemo/JjwtdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.codepure.jjwtdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjwtdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
